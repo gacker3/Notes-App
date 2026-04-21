8c3c909358e471b84ea0c5ec43cd76454994f26f
gacker3 <109584464+gacker3@users.noreply.github.com>
2026-04-21 10:10:03 -0400

    Delete preload.js

--- preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  toggleApp:  () => ipcRenderer.send('toggle-app'),
-  closeApp:   () => ipcRenderer.send('close-app'),
-  onAppState: (cb) => ipcRenderer.on('app-state', (_event, isOpen) => cb(isOpen)),
-  saveData:   (data) => ipcRenderer.send('save-data', data),
-  loadData:   () => ipcRenderer.invoke('load-data')
-});

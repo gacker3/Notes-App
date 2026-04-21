978d4947aadf078fdf870217e53d44f2bd83e716
gacker3 <109584464+gacker3@users.noreply.github.com>
2026-04-21 10:10:38 -0400

    Add files via upload

---
+++ preload.js
@@ -0,0 +1,12 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  toggleApp:       () => ipcRenderer.send('toggle-app'),
+  closeApp:        () => ipcRenderer.send('close-app'),
+  onAppState:      (cb) => ipcRenderer.on('app-state', (_event, isOpen) => cb(isOpen)),
+  saveData:        (data) => ipcRenderer.send('save-data', data),
+  loadData:        () => ipcRenderer.invoke('load-data'),
+  openGroup:       (group) => ipcRenderer.send('open-group', group),
+  onOpenGroup:     (cb) => ipcRenderer.on('navigate-group', (_event, group) => cb(group)),
+  syncFavourites:  (favs) => ipcRenderer.send('sync-favourites', favs),
+});
